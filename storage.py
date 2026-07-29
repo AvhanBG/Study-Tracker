60cdb3b5cb5f14ada5cf5d134459d89108e4a87e
AvhanBG <avhanblake25@gmail.com>
2026-07-29 22:14:08 +0100

    added SQLite database

---
+++ storage.py
@@ -0,0 +1,53 @@
+import sqlite3
+
+print("storage.py imported")
+
+class Storage:
+
+    def __init__(self):
+        print("Storage __init__ running")
+
+        self.connection = sqlite3.connect("storage.db")
+        self.create_table()
+
+
+    def create_table(self):
+
+        print("Creating table")
+
+        cursor = self.connection.cursor()
+
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS sessions (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            date TEXT NOT NULL,
+            start_time TEXT NOT NULL,
+            end_time TEXT NOT NULL,
+            duration_minutes INTEGER NOT NULL
+        )
+        """)
+
+        self.connection.commit()
+
+    def save_session(self, session):
+        cursor = self.connection.cursor()
+        cursor.execute("""
+        INSERT INTO sessions
+        (date, start_time, end_time, duration_minutes)
+        VALUES (?, ?, ?, ?)
+        """,
+        (
+            str(session.date),
+            str(session.start_time),
+            str(session.end_time),
+            str(session.duration_minutes)
+        ))
+        self.connection.commit()
+
+    def load_sessions(self):
+        cursor = self.connection.cursor()
+        cursor.execute("""
+        SELECT * FROM sessions
+        """)
+        sessions = cursor.fetchall()
+        return sessions
